ca9587003dee326f63bee72e7dbd42a80cbea18e
Cleiton <cleiton.p.marques@gmail.com>
2020-04-20 11:08:26 -0300

    Inclusão de métodos para inserir no banco

--- database.js
+++ database.js
@@ -3,6 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 let db_uri = config.dbConfig.dbUrl;
 let database;
+
 MongoClient.connect(db_uri, {  
   poolSize: 10
 },function(err, db) {
@@ -10,5 +11,21 @@ MongoClient.connect(db_uri, {
     database=db;
     }
 );
+
+const createStorage = (dbname, dbstorage) => {
+  dbo.createCollection(dbstorage, function(err, res) {
+    if (err) throw err;
+    console.log(`Storage (table or collection) created: ${res}`);
+  });
+}
+
+const insert = (dbname, dbstorage, obj) => {
+  let dbo = db.db(dbname);
+
+  dbo.collection(dbstorage).insertOne(obj, function(err, res) {
+    if (err) throw err;
+    console.log(`Data stored: ${res}`);
+  });
+}
 
 module.exports = database;
