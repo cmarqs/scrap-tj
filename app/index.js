b2c5f26275f7cee91912f0686a56d3cd657456d6
Cleiton <cleiton.p.marques@gmail.com>
2020-04-22 17:20:46 -0300

    chamada separada para puxar lista de sites

--- app/index.js
+++ app/index.js
@@ -2,13 +2,20 @@ const scrap = require('./scrap');
 
 const runScraper = async () => {
     const lstSites = getSiteList();
+
+    if (!lstSites)
+        throw new Error('Não foi recuperada lista de sites para raspar')
     const newsList = await scrap.doScrap(lstSites);
     console.log(newsList)
     console.log(`Total páginas extraídas: ${newsList.length}`);
 }
 
 const getSiteList = () => {
-
+    const sites = //require('../database/sites');
+        arr = [];
+    arr.push(sites.sites[0]);
+    console.log(arr);
+    return arr;
 }
 
 exports.runScraper = runScraper;
