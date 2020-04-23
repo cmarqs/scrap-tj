e279c9bea226503b7d7fd37b70f5980c41443deb
Cleiton <cleiton.p.marques@gmail.com>
2020-04-23 09:21:03 -0300

    Desfeito o 'Fecha conexão ao terminar execução'

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const sitecontroller = require('./controllers/sites.controller')
 
 void (async () => {
 	await db.connect('tjnews');
-	// sitecontroller.findSite({ url: 'http://www.tjsp.jus.br/Noticias' }, function (sites) {
-	// 	console.log(`Resposta: ${JSON.stringify(sites)} \n`);
-	// });
-	//scrap.runScraper();
-
-	db.close();
+	scrap.runScraper();
 })();
 
 
